08651405aab924bebd0cedadf3654d6d0f0bd69d
Evandikt <evandikt@gmail.com>
2021-08-29 14:33:28 +0200

    Update to @grpc && add stream example

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const grpc = require('grpc')
-const notesProto = grpc.load('notes.proto')
+const grpcLibrary = require('@grpc/grpc-js')
+const protoLoader = require('@grpc/proto-loader')
 const uuidv1 = require('uuid/v1')
 
-const server = new grpc.Server()
+
+const notesProtoLoad = protoLoader.loadSync('notes.proto')
+const notesProto = grpcLibrary.loadPackageDefinition(notesProtoLoad);
+
+const server = new grpcLibrary.Server()
 const notes = [
     { id: '1', title: 'Note 1', content: 'Content 1'},
     { id: '2', title: 'Note 2', content: 'Content 2'}
@@ -10,15 +14,21 @@ const notes = [
 
 server.addService(notesProto.NoteService.service, {
     list: (_, callback) => {
-        callback(null, notes)
+        callback(null, { notes })
+    },
+    listSteam: (call) => {
+        notes.forEach((i) => {
+            call.write(i)
+        })
+        call.end()
     },
     get: (call, callback) => {
-        let note = notes.find((n) => n.id == call.request.id)
+        let note = notes.find((n) => n.id === call.request.id)
         if (note) {
             callback(null, note)
         } else {
             callback({
-                code: grpc.status.NOT_FOUND,
+                code: grpcLibrary.status.NOT_FOUND,
                 details: "Not found"
             })
         }
@@ -30,33 +40,33 @@ server.addService(notesProto.NoteService.service, {
         callback(null, note)
     },
     update: (call, callback) => {
-        let existingNote = notes.find((n) => n.id == call.request.id)
+        let existingNote = notes.find((n) => n.id === call.request.id)
         if (existingNote) {
             existingNote.title = call.request.title
             existingNote.content = call.request.content
-            callback(null, existingNote)         
+            callback(null, existingNote)
         } else {
             callback({
-                code: grpc.status.NOT_FOUND,
+                code: grpcLibrary.status.NOT_FOUND,
                 details: "Not found"
             })
         }
     },
     delete: (call, callback) => {
-        let existingNoteIndex = notes.findIndex((n) => n.id == call.request.id)
-        if (existingNoteIndex != -1) {
+        let existingNoteIndex = notes.findIndex((n) => n.id === call.request.id)
+        if (existingNoteIndex !== -1) {
             notes.splice(existingNoteIndex, 1)
             callback(null, {})
         } else {
             callback({
-                code: grpc.status.NOT_FOUND,
+                code: grpcLibrary.status.NOT_FOUND,
                 details: "Not found"
             })
         }
     }
 })
 
-server.bind('127.0.0.1:50051',
-  grpc.ServerCredentials.createInsecure())
-console.log('Server running at http://127.0.0.1:50051')
-server.start()
+server.bindAsync('127.0.0.1:5005', grpcLibrary.ServerCredentials.createInsecure(), (args) => {
+    console.log('Server running at http://127.0.0.1:5005', args)
+    server.start()
+})
